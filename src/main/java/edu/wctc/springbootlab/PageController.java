cb23ae9939a156553ce96cc36c2c4285b229f746
bryczek23 <Bryczek1@my.wctc.edu>
2022-08-31 13:51:38 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/springbootlab/PageController.java
@@ -0,0 +1,23 @@
+package edu.wctc.springbootlab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class PageController {
+    @GetMapping("view-cart")
+    public String showCart() { return "/orders/cart"; }
+
+    @PostMapping("place-order")
+    public String redirectOrder() {return "redirect:/"; }
+
+    @RequestMapping("/product/detail")
+    public String showProductPage() { return "/catalog/product-detail"; }
+
+    @RequestMapping("search")
+    public String redirectGoogle() { return "redirect:http://www.google.com"; }
+
+    @RequestMapping("/")
+    public String showHomePage() { return "index"; }
+
+}
